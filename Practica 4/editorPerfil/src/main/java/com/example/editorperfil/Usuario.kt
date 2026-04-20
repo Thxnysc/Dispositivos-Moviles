63204a7e123a233d19a482018c5d6c471b04e8e3
Thxnysc <acervantesc@ulasalle.edu.pe>
2026-04-20 01:11:11 -0500

    Delete
    Se borro todo por error de envio (carpeta equivocada)

--- Practica 4/editorPerfil/src/main/java/com/example/editorperfil/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package com.example.editorperfil
-import java.io.Serializable
-
-data class Usuario(
-    val nombre: String,
-    val edad: String,
-    val ciudad: String,
-    val correo: String
-) : Serializable
